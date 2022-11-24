bf46d2b49e06edc4ca0a9dbb863cef02ccec5463
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2022-11-24 16:33:08 +0100

    did minor tweaks

--- main_console/src/length_of/lengths.rs
+++ main_console/src/length_of/lengths.rs
@@ -1,18 +1,44 @@
-struct Sentences<'a>{line: Vec<&'a str>}
+use std::collections::HashMap;
+
+struct Sentences<'a> {
+    line: Vec<&'a str>,
+}
+
+type SentenceTuple<'a> = (&'a str, usize);
+
+trait PrintLengths{
+    fn print_length(&self);
+}
 
 trait GetLengths {
-    fn lengths(&self) -> Vec<(&str, usize)>;
+    fn lengths(&self) -> Vec<SentenceTuple>;
 }
 
 impl<'a> GetLengths for Sentences<'a> {
-    fn lengths(&self) -> Vec<(&str, usize)> {
+    fn lengths(&self) -> Vec<SentenceTuple> {
         let lengths = self.line.iter().map(|x| x.len()).collect::<Vec<usize>>();
-        self.line.clone().into_iter().zip(lengths).collect::<Vec<(&str, usize)>>()
+        return self
+            .line
+            .clone()
+            .into_iter()
+            .zip(lengths)
+            .collect::<Vec<SentenceTuple>>();
+    }
+}
+
+impl PrintLengths for Vec<SentenceTuple<'_>>{
+    fn print_length(&self) {
+        
     }
 }
 
 
-pub fn print_length() {
-    let  input_array = Sentences{line: vec!["Hi", "hello", "Hi i'm nav"]};
-    println!("{:?}", input_array.lengths());
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn length_tuple() {
+        let input_array = Sentences{line: vec!["", " ", "Hi i'm nav."],};
+        assert_eq!(input_array.lengths(), vec![("", 0), (" ", 1), ("Hi i'm nav.", 11)]);
+    }
 }
--- main_console/src/main.rs
+++ main_console/src/main.rs
@@ -1,19 +1,6 @@
-
 mod length_of;
-//use length_of::lengths::print_str_length;
 use length_of::lengths::print_length;
 
 fn main() {
     print_length();
-}
-
-
-#[cfg(test)]
-mod test {
-    //use super::*;
-
-    #[test]
-    fn equals_2() {
-        assert_eq!(2, 2);
-    }
 }
