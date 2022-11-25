fc8a19afab3cec49fefbfb99c822e54b74f07f29
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2022-11-25 12:17:13 +0100

    Added Sentences.longest()

--- main_console/src/length_of/lengths.rs
+++ main_console/src/length_of/lengths.rs
@@ -1,29 +1,51 @@
-use std::collections::HashMap;
 use crate::Sentences;
+use std::collections::HashMap;
 
 type SentenceLengths<'a> = HashMap<&'a str, usize>;
 pub trait GetLengths {
-    fn lengths(&self) -> SentenceLengths;
+    fn lengths(&self) -> Result<SentenceLengths, &'static str>;
+}
+
+pub trait GetLongestItem {
+    fn longest(&self) -> Result<&str, &'static str>;
 }
 
 impl<'a> GetLengths for Sentences<'a> {
-    fn lengths(&self) -> SentenceLengths {
-        let lengths = self.line.iter().map(|x| x.len()).collect::<Vec<usize>>();
-        return self
-            .line
-            .clone()
-            .into_iter()
-            .zip(lengths)
-            .collect::<SentenceLengths>();
+    fn lengths(&self) -> Result<SentenceLengths, &'static str> {
+        match self.line.len() {
+            0 => Err("Vector is empty"),
+            _ => {
+                let lengths = self.line.iter().map(|x| x.len()).collect::<Vec<usize>>();
+                //Is used to get each induvidual length so it can be zipped together and returned
+                Ok(self
+                    .line
+                    .clone()
+                    .into_iter()
+                    .zip(lengths)
+                    .collect::<SentenceLengths>())
+            }
+        }
     }
 }
 
-pub fn print_lengths(input: &Sentences) {
-    for (sentence, length) in input.lengths(){
-        println!("\"{}\", is: {} long.", sentence, length)
+impl<'a> GetLongestItem for Sentences<'a> {
+    fn longest(&self) -> Result<&str, &'static str> {
+        match self.line.len() {
+            0 => Err("Vector is empty"),
+            _ => {
+                //finds the longest item in vectors of type Sentences
+                let longest = self.line.iter().fold(self.line[0], |acc, &item| {
+                    if item.len() > acc.len() {
+                        item
+                    } else {
+                        acc
+                    }
+                });
+                Ok(longest)
+            }
+        }
     }
 }
-
 
 #[cfg(test)]
 mod test {
@@ -31,20 +53,21 @@ mod test {
 
     #[test]
     fn empty() {
-        let string_lengths = Sentences {line: vec![""]};
-        assert_eq!(string_lengths.lengths()[""],0);
+        let string_lengths = Sentences { line: vec![""] };
+        assert_eq!(string_lengths.lengths().unwrap()[""], 0);
     }
 
     #[test]
     fn whitespace() {
-        let string_lengths = Sentences {line: vec![" "]};
-        assert_eq!(string_lengths.lengths()[" "],1);
+        let string_lengths = Sentences { line: vec![" "] };
+        assert_eq!(string_lengths.lengths().unwrap()[" "], 1);
     }
 
     #[test]
     fn special_characters() {
-        let string_lengths = Sentences {line: vec!["&'\n\\."]};
-        assert_eq!(string_lengths.lengths()["&'\n\\."],5);
+        let string_lengths = Sentences {
+            line: vec!["&'\n\\."],
+        };
+        assert_eq!(string_lengths.lengths().unwrap()["&'\n\\."], 5);
     }
 }
-
--- main_console/src/main.rs
+++ main_console/src/main.rs
@@ -1,11 +1,22 @@
 mod length_of;
-use length_of::lengths::print_lengths;
+use length_of::lengths::{GetLengths, GetLongestItem};
 
 pub struct Sentences<'a> {
     line: Vec<&'a str>,
 }
 
-
 fn main() {
-    print_lengths(&Sentences{line: vec!["", " ", "Hi i'm nav."]});
+    let input_vec = Sentences { line: vec![" ", "hi i'm nav"] };
+
+    let lengths_hash = &input_vec.lengths();
+    match lengths_hash {
+        Ok(hash) => println!("All the items in the hashmap are: {:?}", hash),
+        Err(err) => println!("There was an error: {:?}", err),
+    }
+
+    let longest_str = &input_vec.longest();
+    match longest_str {
+        Ok(str) => println!("[{:?}] Is the longest.", str),
+        Err(err) => println!("There was an error: {:?}", err),
+    }
 }
